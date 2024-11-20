6cd17edfab5bad22329d2d12e4a3402c8fc04539
volodg <volodymyr.gorbenko@eqvilent.com>
2024-11-20 22:14:38 +0100

    Add unit test and fix bug

--- hash_table/src/doubly_linked_list.rs
+++ hash_table/src/doubly_linked_list.rs
@@ -52,7 +52,7 @@ impl<T> DoublyLinkedList<T> {
 
     // TODO review
     pub fn pop_front(&mut self) -> Option<T> {
-        self.head.take().map(|old_head| {
+        self.head.take().and_then(|old_head| {
             if let Some(next) = old_head.borrow_mut().next.take() {
                 next.borrow_mut().prev = Weak::new();
                 self.head = Some(next);
@@ -60,7 +60,8 @@ impl<T> DoublyLinkedList<T> {
                 self.tail = None;
             }
             self.length -= 1;
-            Rc::try_unwrap(old_head).ok().unwrap().into_inner().value
+            let node = Rc::try_unwrap(old_head).ok()?.into_inner();
+            Some(node.value)
         })
     }
 
@@ -78,6 +79,9 @@ impl<T> DoublyLinkedList<T> {
         } else {
             self.tail = node_ref.prev.upgrade();
         }
+
+        node_ref.next = None;
+        node_ref.prev = Default::default();
 
         self.length -= 1;
     }
@@ -100,5 +104,115 @@ impl<T> DoublyLinkedList<T> {
 impl<T> Drop for DoublyLinkedList<T> {
     fn drop(&mut self) {
         while self.pop_front().is_some() {}
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_push_back_and_back() {
+        let mut list = DoublyLinkedList::new();
+        list.push_back(1);
+        list.push_back(2);
+        list.push_back(3);
+
+        assert_eq!(*list.back().unwrap(), 3);
+        assert_eq!(list.length, 3);
+    }
+
+    #[test]
+    fn test_pop_front() {
+        let mut list = DoublyLinkedList::new();
+        list.push_back(1);
+        list.push_back(2);
+        list.push_back(3);
+
+        assert_eq!(list.pop_front(), Some(1));
+        assert_eq!(list.pop_front(), Some(2));
+        assert_eq!(list.pop_front(), Some(3));
+        assert_eq!(list.pop_front(), None);
+        assert_eq!(list.length, 0);
+    }
+
+    #[test]
+    fn test_remove_node() {
+        let mut list = DoublyLinkedList::new();
+        {
+            let node1 = list.push_back(1);
+            let node2 = list.push_back(2);
+            let node3 = list.push_back(3);
+
+            // Remove the middle node
+            list.remove(node2);
+        }
+        assert_eq!(list.length, 2);
+
+        // Check remaining nodes
+        assert_eq!(list.pop_front(), Some(1));
+        assert_eq!(list.pop_front(), Some(3));
+        assert_eq!(list.pop_front(), None);
+    }
+
+    #[test]
+    fn test_front_and_back() {
+        let mut list = DoublyLinkedList::new();
+        list.push_back(1);
+        list.push_back(2);
+        list.push_back(3);
+
+        assert_eq!(*list.front().unwrap(), 1);
+        assert_eq!(*list.back().unwrap(), 3);
+    }
+
+    #[test]
+    fn test_empty_list() {
+        let list: DoublyLinkedList<i32> = DoublyLinkedList::new();
+        assert!(list.front().is_none());
+        assert!(list.back().is_none());
+        assert_eq!(list.length, 0);
+    }
+
+    #[test]
+    fn test_remove_head() {
+        let mut list = DoublyLinkedList::new();
+        let node1 = list.push_back(1);
+        list.push_back(2);
+        list.push_back(3);
+
+        list.remove(node1);
+        assert_eq!(list.length, 2);
+
+        assert_eq!(list.pop_front(), Some(2));
+        assert_eq!(list.pop_front(), Some(3));
+        assert_eq!(list.pop_front(), None);
+    }
+
+    #[test]
+    fn test_remove_tail() {
+        let mut list = DoublyLinkedList::new();
+        list.push_back(1);
+        list.push_back(2);
+        let node3 = list.push_back(3);
+
+        list.remove(node3);
+        assert_eq!(list.length, 2);
+
+        assert_eq!(list.pop_front(), Some(1));
+        assert_eq!(list.pop_front(), Some(2));
+        assert_eq!(list.pop_front(), None);
+    }
+
+    #[test]
+    fn test_remove_single_node() {
+        let mut list = DoublyLinkedList::new();
+        let node1 = list.push_back(1);
+
+        list.remove(node1);
+        assert_eq!(list.length, 0);
+
+        assert!(list.front().is_none());
+        assert!(list.back().is_none());
     }
 }
